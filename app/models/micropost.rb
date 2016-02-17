6f5ba709cd9ed40e8a3f73ff734d1943c9e4846a
Su <sp106168@gmail.com>
2016-02-17 22:20:25 +0800

    添加认证存在性和长度验证

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ActiveRecord::Base
+  #限制内容最大值已经设置存在性
+  validates :content,length: {maximum: 255},presence: true
+  #设置属于一个用户约束
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  #设置拥有多个微博
+  has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
